d929b981c64027d18bae372fd84de7b02998f150
LalaLero83 <168014695+LalaLero83@users.noreply.github.com>
2024-05-17 12:54:00 -0300

    Validacion form de contacto.js

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
 document.querySelector('.menu-icon').addEventListener('click', function() {
     document.querySelector('.menu').classList.toggle('active');
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('miFormulario').addEventListener('submit', function(event) {
+        var nombre = document.getElementById('nombre').value.trim();
+        var apellido = document.getElementById('apellido').value.trim();
+        var email = document.getElementById('email').value.trim();
+        var telefono = document.getElementById('telefono').value.trim();
+
+        if (!nombre || !apellido || !email || !telefono) {
+            event.preventDefault();
+            swal('Error', 'Por favor, completa todos los campos obligatorios.', 'error');
+        } else if (!/^\d+$/.test(telefono)) {
+            event.preventDefault();
+            swal('Error', 'Por favor, ingresa solo números en el campo de teléfono.', 'error');
+        } else {
+            // Si todos los campos están completos y el teléfono es válido, se muestra el mensaje de éxito
+            swal({
+                title: 'Éxito',
+                text: 'Su mensaje ha sido enviado.',
+                icon: 'success',
+                buttons: ['Cancelar', 'OK'], // Agregamos botones para confirmación
+            }).then(function(confirm) {
+                if (confirm) {
+                    // Si el usuario hace clic en "OK", se envía el formulario
+                    document.getElementById('miFormulario').submit();
+                }
+            });
+            event.preventDefault(); // Evita que el formulario se envíe automáticamente
+        }
+    });
+});
