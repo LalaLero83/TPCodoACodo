d652aca89409a705e48e4bfd7dcd55b5f5b33e7a
LalaLero83 <168014695+LalaLero83@users.noreply.github.com>
2024-05-23 12:43:05 -0300

    validar terminos y condiciones.js

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ document.addEventListener('DOMContentLoaded', function() {
         var apellido = document.getElementById('apellido').value.trim();
         var email = document.getElementById('email').value.trim();
         var telefono = document.getElementById('telefono').value.trim();
-          var terminos = document.getElementById('terminos').value.trim();
+        var terminos = document.getElementById('terminos').checked;
 
-
-        if (!nombre || !apellido || !email || !telefono || !terminos ) {
+        if (!nombre || !apellido || !email || !telefono) {
             event.preventDefault();
             swal('Error', 'Por favor, completa todos los campos obligatorios.', 'error');
         } else if (!/^\d+$/.test(telefono)) {
             event.preventDefault();
             swal('Error', 'Por favor, ingresa solo números en el campo de teléfono.', 'error');
+        } else if (!terminos) {
+            event.preventDefault();
+            swal('Error', 'Debes aceptar los términos y condiciones.', 'error');
         } else {
             swal({
                 title: 'Éxito',
